afe4388efd60a7ce7029bb6f6f00c6cfff388484
EbukaNweje <nwejeebuka@gmail.com>
2025-02-06 07:57:41 -0800

    D

---
+++ src/Auth/Login.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react' 
+import "../Auth/LoginStyle.css"
+import { IoMdArrowDropleft } from "react-icons/io";
+import { FaRegUser } from "react-icons/fa";
+import { FaRegEye } from "react-icons/fa6";
+
+
+const Login = () => {
+    const [showPassword, setShowPassword] = useState(false)
+
+    const changePassword = () => { 
+        setShowPassword(!showPassword)
+    }
+
+  return (
+    <div className='MainBody'>
+        <div className='Wrapper'>
+            <div className='navBack'>
+                <IoMdArrowDropleft/>
+            </div>
+            
+            <div className='AuthHader'>
+                <h3>Let’s login.</h3>
+                <p>Let’s us know what your mobile and your password</p>
+            </div>
+
+                <form className='MyForm'>
+                    <div className='MyFormWrapper'>
+                        <div className='divInputWrapper'> 
+                            <div className='divInput'>
+                                <FaRegUser className='IconFont'/>
+                                <input type="text"  className='Input'/>
+                            </div>
+                            <div className='divInput'>
+                                <input type={showPassword ? "text" : "password"}  className='Input'/>
+                                <FaRegEye className='IconFont' onClick={changePassword}/>
+                            </div>
+                        </div>
+                        <div className='forgot'>
+                            <p>Forgot password?</p>
+                        </div>
+                            <button className='Btn'>Sign In</button>
+
+                            <div className='Not'>
+                            <p>Not a member? Create an new account? <span>Sign Up</span></p>
+                        </div>
+                    </div>
+                </form> 
+
+        </div>
+    </div>
+  )
+}
+
+export default Login
