289be0b0a5f7f01c27c4dcf0b4205aa55a67f7c4
EbukaNweje <nwejeebuka@gmail.com>
2025-02-07 02:04:59 -0800

    F

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import Login from './Auth/Login'
+import {RouterProvider, createHashRouter} from "react-router-dom";
+import Signup from './Auth/Signup';
 
 const App = () => {
+  const router = createHashRouter([
+    {
+      path : "/",
+      element: <Login/>
+    },
+    {
+      path : "/Sign-up",
+      element: <Signup/>
+    }
+  ])
   return (
-    <div>
-      <Login/>
-    </div>
+    <>
+            <RouterProvider router={router} />
+    </>
   )
 }
 
--- src/Auth/Signup.jsx
+++ src/Auth/Signup.jsx
@@ -5,9 +5,9 @@ import { IoMdArrowDropleft } from "react-icons/io";
 import { FaRegUser, FaRegEyeSlash } from "react-icons/fa";
 import { FaRegEye } from "react-icons/fa6";
 import { useNavigate } from 'react-router-dom';
+import { MdOutlineAttachEmail } from "react-icons/md";
 
-
-const Login = () => {
+const Signup = () => {
     const [showPassword, setShowPassword] = useState(false)
     const Nav = useNavigate()
     const changePassword = () => { 
@@ -22,8 +22,8 @@ const Login = () => {
             </div>
             
             <div className='AuthHader'>
-                <h3>Let’s login.</h3>
-                <p>Let’s us know what your mobile and your password</p>
+                <h3>Create an Account.</h3>
+                <p>Please enter your appropriate details to continue</p>
             </div>
 
                 <form className='MyForm'>
@@ -34,6 +34,12 @@ const Login = () => {
                                 <input type="text"  className='Input' placeholder='Enter your mobile'/>
                             </div>
                             <div className='divInput'>
+                                 <MdOutlineAttachEmail className='IconFont'/>
+                                <input type="text" className='Input' placeholder='Enter your invite code'/>
+                            </div>
+                        </div>
+                        <div className='divInputWrapper'> 
+                        <div className='divInput'>
                                 <input type={showPassword ? "text" : "password"}  className='Input' placeholder='Enter your password'/>
                                 {
                                     showPassword ? <FaRegEyeSlash onClick={changePassword}/> : <FaRegEye className='IconFont' onClick={changePassword}/>
@@ -41,14 +47,26 @@ const Login = () => {
                                
                                 
                             </div>
+                            <div className='divInput'>
+                                <input type={showPassword ? "text" : "password"}  className='Input' placeholder='Confirm password'/>
+                                {
+                                    showPassword ? <FaRegEyeSlash onClick={changePassword}/> : <FaRegEye className='IconFont' onClick={changePassword}/>
+                                }
+                               
+                                
+                            </div>
                         </div>
-                        <div className='forgot'>
-                            <p>Forgot password?</p>
-                        </div>
+                        {/* <div className='divInputWrapper'> 
+                            <div className='divInput'>
+                                <FaRegUser className='IconFont'/>
+                                <input type="text"  className='Input' placeholder='Enter your mobile'/>
+                            </div>
+                        </div> */}
+
                             <button className='Btn'>Sign In</button>
 
                             <div className='Not'>
-                            <p>Not a member?  <small>Create an new account? <span onClick={()=> Nav("/Sign-up")}>Sign Up</span></small></p>
+                            <p>Not a member?  <small>Already have an account?<span onClick={()=> Nav("/")}> Sign in</span></small></p>
                         </div>
                     </div>
                 </form> 
@@ -58,4 +76,4 @@ const Login = () => {
   )
 }
 
-export default Login
+export default Signup
