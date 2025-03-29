4df321575d470390539c997345853f464f060e18
EbukaNweje <nwejeebuka@gmail.com>
2025-03-29 11:32:03 -0700

    DG

--- src/pages/Testing.jsx
+++ src/pages/Testing.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import "./testing.css"
+import { useNavigate } from 'react-router-dom';
 // import 'animate.css';
 
 const Testing = () => {
+  const Nav = useNavigate();
   return (
     <div className='MainWrapper'>
       <div className='wrapper'>
@@ -11,7 +13,7 @@ const Testing = () => {
 Empowering Connections, Elevating Growth!
 </h1>
 <p className='animate__animated animate__fadeInLeft'>YATiCare Is A Powerful Community-Driven Financial Model, <br /> birthed for the purpose of Creating Connections and Enhancing Financial Growth amongst <br /> Individuals and Businesses.</p>
-        <button className='Btn'>Get Started</button>
+        <button className='Btn' onClick={() => Nav("/signup")}>Get Started</button>
         </div>
         <div className='HeroPageRight'>
             {/* <img src="src/assets/grow.jpg" alt="" className='imge' /> */}
